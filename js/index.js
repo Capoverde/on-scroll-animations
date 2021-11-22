0f5f414038fe9b6714ffd89ed6bd825db47b6a56
Piotr <piotr_as@yahoo.com>
2021-11-22 14:27:05 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector('.navbar')
+
+
+// window.addEventListener('scroll', function(e){
+//     if(document.pageYoffset >= 200){
+//         nav.style.background = 'red';
+//     }
+//     console.log('scroling')
+// })
